29cc7ddcce776d95102c158abb1a7c3f693be023
Pietro Di Bello <pierodibello@gmail.com>
2017-06-06 23:46:57 +0200

    replaces multiple template expressions

--- src/main/java/it/pierodibello/kata/TemplateEngine.java
+++ src/main/java/it/pierodibello/kata/TemplateEngine.java
@@ -5,8 +5,9 @@ import java.util.HashMap;
 public class TemplateEngine {
     public String evaluate(String templateString, HashMap<String, String> variables) {
         for (String key : variables.keySet()) {
-            if (templateString.contains("{$" + key + "}")) {
-                templateString = templateString.replace("{$name}", variables.get(key));
+            String expression = "{$" + key + "}";
+            if (templateString.contains(expression)) {
+                templateString = templateString.replace(expression, variables.get(key));
             }
         }
         return templateString;
--- src/test/java/it/pierodibello/kata/TemplateEngineTest.java
+++ src/test/java/it/pierodibello/kata/TemplateEngineTest.java
@@ -10,10 +10,12 @@ import static org.junit.Assert.assertEquals;
 public class TemplateEngineTest {
 
     private TemplateEngine templateEngine;
+    private HashMap<String, String> variables;
 
     @Before
     public void setUp() throws Exception {
         templateEngine = new TemplateEngine();
+        variables = new HashMap<>();
     }
 
     @Test
@@ -24,12 +26,21 @@ public class TemplateEngineTest {
     }
 
     @Test
-    public void replaces_a_single_variable_in_the_template_with_its_value() throws Exception {
-        HashMap<String, String> variables = new HashMap<>();
+    public void replaces_a_single_variable_expression_in_the_template_with_its_value() throws Exception {
         variables.put("name", "Pietro");
 
         String result = templateEngine.evaluate("Hi {$name}!", variables);
 
         assertEquals("Hi Pietro!", result);
+    }
+
+    @Test
+    public void replaces_multiple_variable_expressions_in_the_template_with_their_values() throws Exception {
+        variables.put("name", "Pietro");
+        variables.put("surname", "Di Bello");
+
+        String result = templateEngine.evaluate("Hi {$name} {$surname}!", variables);
+
+        assertEquals("Hi Pietro Di Bello!", result);
     }
 }
