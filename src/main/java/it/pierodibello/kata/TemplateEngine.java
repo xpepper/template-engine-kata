da8f184fb988360436520ca40e7946b050f2047e
Pietro Di Bello <pierodibello@gmail.com>
2017-06-06 23:38:45 +0200

    refactors to handle all possible single template variables

--- src/main/java/it/pierodibello/kata/TemplateEngine.java
+++ src/main/java/it/pierodibello/kata/TemplateEngine.java
@@ -1,7 +1,14 @@
 package it.pierodibello.kata;
 
+import java.util.HashMap;
+
 public class TemplateEngine {
-    public String evaluate(String templateString) {
+    public String evaluate(String templateString, HashMap<String, String> variables) {
+        for (String key : variables.keySet()) {
+            if (templateString.contains("{$" + key + "}")) {
+                templateString = templateString.replace("{$name}", variables.get(key));
+            }
+        }
         return templateString;
     }
 }
--- src/test/java/it/pierodibello/kata/TemplateEngineTest.java
+++ src/test/java/it/pierodibello/kata/TemplateEngineTest.java
@@ -3,8 +3,9 @@ package it.pierodibello.kata;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class TemplateEngineTest {
 
@@ -17,8 +18,18 @@ public class TemplateEngineTest {
 
     @Test
     public void string_with_no_template_variables_transforms_to_the_very_same_string() throws Exception {
-        String result = templateEngine.evaluate("Hi There!");
+        String result = templateEngine.evaluate("Hi There!", new HashMap<>());
 
         assertEquals("Hi There!", result);
+    }
+
+    @Test
+    public void replaces_a_single_variable_in_the_template_with_its_value() throws Exception {
+        HashMap<String, String> variables = new HashMap<>();
+        variables.put("name", "Pietro");
+
+        String result = templateEngine.evaluate("Hi {$name}!", variables);
+
+        assertEquals("Hi Pietro!", result);
     }
 }
